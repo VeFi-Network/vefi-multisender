8e7151ca102e9ab70eb78e0e3e03584b45c24dc5
Kingsley Victor <javaprodigy56@gmail.com>
2022-04-27 16:29:49 +0100

    write test

--- contracts/Multisender.sol
+++ contracts/Multisender.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.0;
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 import './libraries/Helpers.sol';
 
-contract MultiSender is Ownable {
+contract MultiSender is Ownable, ReentrancyGuard {
   using SafeMath for uint256;
 
   struct Payment {
@@ -64,7 +65,7 @@ contract MultiSender is Ownable {
     return _index;
   }
 
-  function multisend(Payment[] memory _payments, address _token) external payable returns (bool) {
+  function multisend(Payment[] memory _payments, address _token) external payable nonReentrant returns (bool) {
     uint256 _fee = calcFee(_payments.length);
     uint256 _totalAmount;
 
